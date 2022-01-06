fa61076db4381e79d75d21763c2468902e7c589f
samvalvi <57542220+samvalvi@users.noreply.github.com>
2022-01-05 18:17:16 -0600

    Tools singleton pattern

---
+++ Log.cs
@@ -0,0 +1,33 @@
+﻿namespace Tools
+{
+    public sealed class Log
+    {
+        private static Log _instance = null;
+        private string _path;
+        //Protección contra multihilos
+        private static object _lock = new object();
+
+        public static Log GetInstance(string path)
+        {
+            //Deja un hilo en espera
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Log(path);
+                }
+            }
+            return _instance;
+        }
+
+        private Log(string path)
+        {
+            _path = path;
+        }
+
+        public void Save(string msg)
+        {
+            File.AppendAllText(_path, msg + Environment.NewLine);
+        }
+    }
+}
